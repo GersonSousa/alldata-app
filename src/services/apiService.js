71d3cdca6d4b80a54eb22b7e2b52febb2b6e3376
Gerson Sousa <agerson3@gmail.com>
2024-09-03 21:38:24 -0300

    Mudando rotas

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3030/api/v1/',
+  baseURL: 'https://alldata-api.onrender.com/api/v1/',
   timeout: 10000,
   withCredentials: true,
   headers: {
