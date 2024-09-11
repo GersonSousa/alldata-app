b108ac41a2d26b4f7fe02c15b9deb09cd3693874
Gerson Sousa <agerson3@gmail.com>
2024-09-10 21:57:57 -0300

    Api service

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import users from '@/services/users';
+import { ref } from 'vue';
+
+const users = ref('');
+
+async function fetchUsers() {
+  const response = await users.getUsers();
+  users.value = response.data;
+}
+
+fetchUsers();
+</script>
 <template>
-  <h1>Olá Mundo</h1>
+  <pre>{{ users }}</pre>
 </template>
 <style scoped></style>
---
+++ src/services/users.js
@@ -0,0 +1,11 @@
+import instance from './apiService';
+
+const getUsers = async () => {
+  try {
+    const response = await instance.get('users');
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
