9be864b05641a981281774f597eda339faacf191
Gerson Sousa <agerson3@gmail.com>
2024-09-19 14:51:33 -0300

    Refatorando

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Cookies from 'js-cookie';
 import Login from '@/pages/Auth/Login.vue';
+import Reset from '@/pages/Auth/Reset.vue';
+import Forgot from '@/pages/Auth/Forgot.vue';
+import NotFound from '@/pages/Error/NotFound.vue';
+
 import Home from '@/pages/Home/Home.vue';
 
 const routes = [
@@ -11,16 +14,39 @@ const routes = [
     meta: { title: 'All | Login' },
   },
   {
+    path: '/reset',
+    name: 'Reset',
+    component: Reset,
+    meta: { title: 'All | Trocar senha' },
+  },
+  {
+    path: '/forgot',
+    name: 'Forgot',
+    component: Forgot,
+    meta: { title: 'All | Esqueci minha senha' },
+  },
+  {
     path: '/home',
     name: 'Home',
     component: Home,
-    meta: { title: 'All | Home', requiresAuth: true },
+    meta: { title: 'All | Home' },
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+    meta: { title: 'All | Página não encontrada' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta?.title ?? 'All';
+  next();
 });
 
 export default router;
